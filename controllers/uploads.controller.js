75c9abbf33c7b15baf744cd09c3df4cbe1074d00
J. Nicolas Aristizabal R <josenicolasaristizabalramirez@gmail.com>
2022-06-03 08:24:01 -0500

    feat: end section 11

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,20 +1,29 @@
+const path = require("path");
+const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const { updateImage } = require("../utils/updateImage");
 
-
 // http://localhost:3005/api/uploads/:type/:id
-// type: entity 
+// type: entity
 // id: id unique
 const fileUploads = (req, res) => {
   const typeAllowed = ["hospitals", "doctors", "users"];
   const extensionAllowed = ["png", "jpg", "jpeg", "gif"];
   const { type, id } = req.params;
-  const file = req.files.image; // body >> form-data (value = file)
-  const nameFile = file.name.split(".");
-  const extension = nameFile[nameFile.length - 1];
+  const file = req.files?.image; // body >> form-data (value = file)
 
   // ---------------------validators---------------------
   // ----------------------------------------------------
+
+  if (!file) {
+    return res.status(400).json({
+      success: false,
+      message: "you should adjunt a img file",
+    });
+  }
+
+  const nameFile = file?.name.split(".");
+  const extension = nameFile[nameFile?.length - 1];
 
   // ------------------------type------------------------
   if (!typeAllowed.includes(type)) {
@@ -57,25 +66,46 @@ const fileUploads = (req, res) => {
 
     // --------------------Update BD-----------------------
     // ----------------------------------------------------
-    
+
     const result = updateImage(type, id, nameFileUpload);
 
-    if(result){
+    if (result) {
       res.status(200).json({
         success: true,
-        message: "file has been upload and assign to "+type,
+        message: "file has been upload and assign to " + type,
         nameFileUpload,
       });
-    }else{
+    } else {
       res.status(200).json({
         success: true,
-        message: "file has been upload, but has been assign to "+type,
+        message: "file has been upload, but has been assign to " + type,
         nameFileUpload,
       });
     }
   });
 };
 
+const showImage = (req, res) => {
+  const type = req.params.type;
+  const urlImg = req.params.urlImg;
+
+  // -----------------------------example endpoint-----------------------------
+
+  // -------------------------search url img in server-------------------------
+  const pathImgSaveInServer = path.join(
+    __dirname,
+    `../uploads/${type}/${urlImg}`
+  );
+
+  // --------------------------------validation--------------------------------
+  if (!fs.existsSync(pathImgSaveInServer)) {
+    const pathImgError = path.join(__dirname, `../uploads/img-error.jpg`);
+    return res.status(404).sendFile(pathImgError);
+  }
+  res.status(200).sendFile(pathImgSaveInServer);
+};
+
 module.exports = {
   fileUploads,
+  showImage,
 };
--- utils/updateImage.js
+++ utils/updateImage.js
@@ -3,6 +3,12 @@ const User = require("../models/users");
 const Doctor = require("../models/doctors.model");
 const Hospital = require("../models/hospitals.model");
 
+const deletePath = (path) => {
+  if (fs.existsSync(path)) {
+    fs.unlinkSync(path);
+  }
+};
+
 const updateImage = async (type, id, nameFileUpload) => {
   switch (type) {
     case "doctors":
@@ -18,17 +24,33 @@ const updateImage = async (type, id, nameFileUpload) => {
       // ------------delete img last------------
       // ---------------------------------------
       const oldPath = `./uploads/doctors/${doctor.img}`;
-      if (fs.existsSync(oldPath)) {
-        fs.unlinkSync(oldPath);
-      }
+      deletePath(oldPath);
 
       doctor.img = nameFileUpload;
       await doctor.save();
       return true;
       break;
     case "hospitals":
+      const hospital = await Hospital.findById(id);
+      if (!hospital) return false;
+
+      const oldPathHosp = `./uploads/hospitals/${hospital.img}`;
+      deletePath(oldPathHosp);
+
+      hospital.img = nameFileUpload; 
+      await hospital.save();
+      return true;
       break;
     case "users":
+      const user = await User.findById(id);
+      if (!user) return false;
+
+      const oldPathUser = `./uploads/users/${user.img}`;
+      deletePath(oldPathUser);
+
+      user.img = nameFileUpload;
+      await user.save();
+      return true;
       break;
   }
 };
