5907ee7d69d1896a9fe9a78cf3806b50994999b8
J. Nicolas Aristizabal R <josenicolasaristizabalramirez@gmail.com>
2022-09-13 23:16:36 -0500

    add heroku config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 require("dotenv").config(); // environments
 let cors = require("cors"); // connection to API
 const { dbConnection } = require("./db/config"); // bd
-const express = require("express"); // server 
+const express = require("express"); // server
 const { logErrors, errroHandler } = require("./middlewares/errorHandler");
 
 // -------------------others config-------------------
@@ -12,7 +12,7 @@ dbConnection(); // ORM bd
 
 // -------------------config server-------------------
 // ---------------------------------------------------
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const ipv4 = " 192.168.0.5:";
 const app = express(); // server
 app.listen(port, () => {
@@ -26,15 +26,14 @@ app.use(cors());
 app.use(express.json());
 
 // ------------connect with the front-end------------
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/api/users', require('./routes/users.router.js'));
-app.use('/api/login', require('./routes/login.router.js'));
-app.use('/api/hospitals', require('./routes/hospitals.router.js'));
-app.use('/api/doctors', require('./routes/doctors.router.js'));
-app.use('/api/all', require('./routes/all.router.js'));
-app.use('/api/uploads', require('./routes/uploads.router.js'));
-
+app.use("/api/users", require("./routes/users.router.js"));
+app.use("/api/login", require("./routes/login.router.js"));
+app.use("/api/hospitals", require("./routes/hospitals.router.js"));
+app.use("/api/doctors", require("./routes/doctors.router.js"));
+app.use("/api/all", require("./routes/all.router.js"));
+app.use("/api/uploads", require("./routes/uploads.router.js"));
 
 // publish postman collection
 // https://learning.postman.com/docs/publishing-your-api/publishing-your-docs/
