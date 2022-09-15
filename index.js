2d30a6aed024cfe6b2055f289113078bb925c444
J. Nicolas Aristizabal R <josenicolasaristizabalramirez@gmail.com>
2022-09-14 22:02:01 -0500

    fixed error about refresh problem

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let cors = require("cors"); // connection to API
 const { dbConnection } = require("./db/config"); // bd
 const express = require("express"); // server
 const { logErrors, errroHandler } = require("./middlewares/errorHandler");
+const path = require("path");
 
 // -------------------others config-------------------
 // ---------------------------------------------------
@@ -35,6 +36,11 @@ app.use("/api/hospitals", require("./routes/hospitals.router.js"));
 app.use("/api/doctors", require("./routes/doctors.router.js"));
 app.use("/api/all", require("./routes/all.router.js"));
 app.use("/api/uploads", require("./routes/uploads.router.js"));
+
+// solve problem about refresh app ( front-end )
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+});
 
 // publish postman collection
 // https://learning.postman.com/docs/publishing-your-api/publishing-your-docs/
